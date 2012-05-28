d9fca5c0e91ab3572a5ebe2b84e82a3fb4b49c0f
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2012-05-28 22:21:22 +0200

    Externalized CommandGroup

---
+++ src/classes/Aspyct/Kickstarter/Shell/CommandGroup.php
@@ -0,0 +1,72 @@
+<?php
+namespace Aspyct\Kickstarter\Shell;
+
+class CommandGroup implements Command {
+    /**
+     * @var array <Command>
+     */
+    private $subcommands;
+    
+    private $name = '<cli>';
+    
+    public function help(array $args=array()) {
+        if (empty($args)) {
+            $this->globalHelp();
+        }
+        else {
+            $this->specificHelp($args);
+        }
+    }
+    
+    private function globalHelp() {
+        echo "Available commands:\n";
+        
+        foreach ($this->subcommands as $command) {
+            printf("  %-14s%s\n", $command->getName(), $command->getBrief());
+        }
+    }
+    
+    private function specificHelp(array $args) {
+        $command = array_shift($args);
+        
+        if (array_key_exists($command, $this->subcommands)) {
+            $this->subcommands[$command]->help($args);
+        }
+        else {
+            echo "No such command: $command\n";
+            $this->globalHelp();
+        }
+    }
+    
+    public function run(array $args) {
+        // $args[0] is the program name
+        $this->name = array_shift($args);
+        
+        if (empty($args)) {
+            $this->help();
+        }
+        else {
+            $command = array_shift($args);
+            
+            if (array_key_exists($command, $this->subcommands)) {
+                $this->subcommands[$command]->run($args);
+            }
+            else {
+                echo "No such command: $command\n";
+                $this->help();
+            }
+        }
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function getBrief() {
+        return 'The main CLI of this software.';
+    }
+    
+    public function addSubCommand(Command $subcommand) {
+        $this->subcommands[$subcommand->getName()] = $subcommand;
+    }
+}
